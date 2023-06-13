a13ee10a75e9e2136099303ee445eb8c31179ac9
Alex Tester <lex.tester93@gmail.com>
2023-06-13 10:48:39 -0400

    Added prompt asking for which character types to use for password as well as while statement and alert if at least one character type is not selected.

--- script.js
+++ script.js
@@ -22,8 +22,24 @@ function generatePassword() {
   var passwordLength = prompt ("Choose a password length of at least 8 characters and no more than 128 characters.");
 }
 
-// Checks to confirm legnth of password is within the parameters
+// Checks to confirm length of password is within the parameters
 while (passwordLength < 8 || passwordLength > 128) {
   alert("Password length must be between 8 and 128 characters!");
   passwordLength = prompt("Choose a password length of at least 8 characters and no more than 128 characters.");
+}
+
+// Asks user which character types to include
+includeLowercase = confirm("Do you want to include lowercase letters?");
+includeUppercase = confirm("Do you want to include uppercase letters?");
+includeNumeric = confirm("Do you want to include numbers?")
+includeSpecial = confirm("Do you want to include special characters?")
+
+
+// Confirms that at least one character type is selected
+while (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
+  alert("At least one character type must be selected!!");
+  includeLowercase = confirm("Do you want to include lowercase letters?");
+  includeUppercase = confirm("Do you want to include uppercase letters?");
+  includeNumeric = confirm("Do you want to include numbers?")
+  includeSpecial = confirm("Do you want to include special characters?")
 }
