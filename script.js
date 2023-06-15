8bfa2c3592575fe01bb04a17d74834fab16b3d17
Alex Tester <lex.tester93@gmail.com>
2023-06-15 16:45:28 -0400

    Added comments so the code is easier to read if you dont know what you're looking for. Changed a few variable names because they flowed better.

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -10,70 +7,69 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
+// Add event listener to generate button, "listens" for the click
 generateBtn.addEventListener("click", writePassword);
 
+// My assignment code is placed here
 // Generates password based on user criteria
 function generatePassword() {
   // Prompts user for the password's criteria
-  var passwordLength = prompt ("Choose a password length of at least 8 characters and no more than 128 characters.");
+  var passwordLength = prompt("Choose a password length of at least 8 characters and no more than 128 characters.");
+
+  // Checks to confirm length of password is within the parameters
+  while (passwordLength < 8 || passwordLength > 128) {
+    alert("Password length must be between 8 and 128 characters!");
+    passwordLength = prompt("Choose a password length of at least 8 characters and no more than 128 characters.");
+  }
+
+  // Asks user which character types to include
+  var includeLowercase = confirm("Do you want to include lowercase letters?");
+  var includeUppercase = confirm("Do you want to include uppercase letters?");
+  var includeNumbers = confirm("Do you want to include numbers?");
+  var includeSpecial = confirm("Do you want to include special characters?");
+
+  // Confirms that at least one character type is selected
+  while (!includeLowercase && !includeUppercase && !includeNumbers && !includeSpecial) {
+    alert("At least one character type must be selected!!");
+    includeLowercase = confirm("Do you want to include lowercase letters?");
+    includeUppercase = confirm("Do you want to include uppercase letters?");
+    includeNumbers = confirm("Do you want to include numbers?");
+    includeSpecial = confirm("Do you want to include special characters?");
+  }
+
+  // Define variables based on criteria
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbersChars = "0123456789";
+  var specialChars = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+
+  // Initialize character set
+  var charSet = '';
+
+  // if statements to add character types if selected
+  if (includeLowercase) {
+    charSet += lowercaseChars;
+  }
+  if (includeUppercase) {
+    charSet += uppercaseChars;
+  }
+  if (includeNumbers) {
+    charSet += numbersChars;
+  }
+  if (includeSpecial) {
+    charSet += specialChars;
+  }
+
+  // Generates the password
+  var password = '';
+  for (var i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * charSet.length);
+    password += charSet[randomIndex];
+  }
+
+  return password;
 }
-
-// Checks to confirm length of password is within the parameters
-while (passwordLength < 8 || passwordLength > 128) {
-  alert("Password length must be between 8 and 128 characters!");
-  passwordLength = prompt("Choose a password length of at least 8 characters and no more than 128 characters.");
-}
-
-// Asks user which character types to include
-includeLowercase = confirm("Do you want to include lowercase letters?");
-includeUppercase = confirm("Do you want to include uppercase letters?");
-includeNumbers = confirm("Do you want to include numbers?")
-includeSpecial = confirm("Do you want to include special characters?")
-
-
-// Confirms that at least one character type is selected
-while (!includeLowercase && !includeUppercase && !includeNumbers && !includeSpecial) {
-  alert("At least one character type must be selected!!");
-  includeLowercase = confirm("Do you want to include lowercase letters?");
-  includeUppercase = confirm("Do you want to include uppercase letters?");
-  includeNumbers = confirm("Do you want to include numbers?")
-  includeSpecial = confirm("Do you want to include special characters?")
-}
-
-// Define variables based on criteria
-var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numbersChars = "0123456789";
-var specialChars = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
-
-// Initialize character set
-var charSet = '';
-
-// if statements to add if chacracter types are selected
-if (includeLowercase) {
-  charset += lowercaseChars;
-}
-if (includeUppercase) {
-  charSet += uppercaseChars;
-}
-if (includeNumbers) {
-  charSet += numbersChars;
-}
-if (includeSpecial) {
-  charSet += specialChars;
-}
-
-// Generates the password
-var password = "";
-for (var i = 0; i < passwordLength; i++)  {
-  var randomIndex = Math.floor(Math.random() * charSet.length)
-  password += charSet[randomIndex];
-}
-
-return password;
 
 
